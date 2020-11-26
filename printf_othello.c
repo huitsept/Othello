7519773603f6b1777a34be451fc876133c7f0bf6
huitsept <70679674+huitsept@users.noreply.github.com>
2020-11-26 22:36:46 +0900

    주석

--- printf_othello.c
+++ printf_othello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "gameboard.h"
-
+//gameboard format 
 void printf_othello()
 {
 	int i, j;
--- winner.c
+++ winner.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "gameboard.h"
 
-void winner(int wc, int wb)
+void winner(int wc, int wb) 
 {
-	if (wc = wb) { printf("무승부입니다.\n"); }
+	if (wc = wb) { printf("무승부입니다.\n"); } //돌의 수로 판 
 
 	printf("%s\n", (wc > wb) ? "승자는 White Player입니다." : "승자는 Black Player입니다.");
 
